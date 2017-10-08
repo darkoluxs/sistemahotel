bcb61791509c118b046a3d68c49ff9d6d3d28f2c
fede <fperalperez@gmail.com>
2017-10-08 18:18:23 -0300

    vistas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,49 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+//VISTAS INDEX
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//VISTAS ALOJAMIENTOS
+
+Route::get('/alojamientos', function() {
+	return view('alojamientos');
+});
+
+//VISTAS SERVICIO AL CLIENTE
+
+Route::get('/serviciocliente',function() {
+	return view('serviciocliente');
+}); 
+
+//VISTAS HABITACIONES
+
+Route::get('/habitaciones', function() { 
+	return view('habitaciones');
+});
+
+//VISTA SERVICIOS
+
+Route::get('/servicios', function() {
+	return view('servicios');
+}); 
+
+//VISTAS FACTURAS 
+
+Route::get('/facturas', function() {
+	return view('facturas');
+}); 
+
+// VISTAS CLIENTES
+
+Route::get('/clientes', function() { 
+	return view('clientes');
+});
+
+//VISTAS EMPLEADOS
+
+Route::get('/empleados' , function () {
+	return view('empleados');
 });
