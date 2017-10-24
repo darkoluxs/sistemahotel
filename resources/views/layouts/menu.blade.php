88cadc3a8a53698cd950bf1ebc0497ef489d6caf
darkoluxs <fperalperez@gmail.com>
2017-10-23 23:08:00 -0300

    agregado css y vistas

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+  <title></title>
+
+<link rel="stylesheet" type="text/css" href="{{'assets/css/menu.css'}}">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+     rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+  <div class="row">
+    <div class="dropdown">
+      <ul>
+        <li>Inicio</li>
+        <li>
+          Servicio al cliente <i class="fa fa-sort-desc" aria-hidden="true"></i>
+          <ul>
+            <li>Reservas</li>
+            <li>Servicios</li>
+            <li><a href="serviciocliente/consumo"> Consumos </a> </li>
+          </ul>
+        </li>
+        <li>Empleados</li>
+        <li>Clientes</li>
+        <li>Factura</li>
+      </ul>
+    </div>
+  </div>
+</div>
+</body>
+</html>
+
+
